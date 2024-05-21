b1f2e5838f33d04ee1038b1e003e6d80e487c236
김지나 <jina4066@naver.com>
2024-05-21 13:36:55 +0900

    feat: CommunityScreen 구현

---
+++ screens/CommunityScreen.jsx
@@ -0,0 +1,19 @@
+import {
+  Image,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import CommunityHeader from "../components/CommunityHeader";
+
+const CommunityScreen = () => {
+  return (
+    <View>
+      <CommunityHeader />
+    </View>
+  );
+};
+
+export default CommunityScreen;
