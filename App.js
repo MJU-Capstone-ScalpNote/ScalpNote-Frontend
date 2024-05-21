a4b1945519e5a561ccc3c483405867f0055482d9
김지나 <jina4066@naver.com>
2024-05-21 13:38:39 +0900

    feat: App.js에 navigation 추가

--- App.js
+++ App.js
@@ -1,10 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import * as MediaLibrary from "expo-media-library";
+import { Camera } from "expo-camera";
+
+import HomeScreen from "./screens/HomeScreen";
+import SignupScreen from "./screens/SignupScreen";
+import LoginScreen from "./screens/LoginScreen";
+import TestHomeScreen from "./screens/TestHomeScreen";
+import CameraScreen from "./screens/CameraScreen";
+import CommunityScreen from "./screens/CommunityScreen";
+import CommunityHeader from "./components/CommunityHeader";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: "홈" }}
+          />
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ title: "로그인" }}
+          />
+          <Stack.Screen
+            name="SignUp"
+            component={SignupScreen}
+            options={{ title: "회원가입" }}
+          />
+          <Stack.Screen
+            name="TestHomeScreen"
+            component={TestHomeScreen}
+            options={{ title: "두피진단" }}
+          />
+          <Stack.Screen
+            name="CameraScreen"
+            component={CameraScreen}
+            options={{ title: "카메라" }}
+          />
+          <Stack.Screen
+            name="CommunityScreen"
+            component={CommunityScreen}
+            options={{ title: "커뮤니티" }}
+          />
+          <Stack.Screen
+            name="CommunityHeader"
+            component={CommunityHeader}
+            // options={{ title: "커뮤니티 헤더" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +66,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
