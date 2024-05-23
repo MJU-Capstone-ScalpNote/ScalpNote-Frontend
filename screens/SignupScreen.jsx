f5a3ec5bb891ee52419fb5db06a34e1e897a55f7
김지나 <jina4066@naver.com>
2024-05-24 04:17:19 +0900

    feat: 회원가입 api 연결

--- screens/SignupScreen.jsx
+++ screens/SignupScreen.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import {
   View,
   Text,
@@ -12,6 +13,30 @@ const SignupScreen = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post(
+        "http://13.209.76.135:8080/auth/sign-up",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+      // 성공적으로 요청이 완료되면 실행될 로직 (예: 화면 전환, 알림 표시 등)
+      console.log(response.data); // 응답 데이터 로깅
+      alert("회원가입이 완료되었습니다.");
+      // 상태 초기화
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      // 에러 처리 로직
+      console.error(error);
+      alert("회원가입 중 오류가 발생했습니다.");
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -38,7 +63,7 @@ const SignupScreen = () => {
         autoCapitalize="none"
       />
 
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
         <Text style={styles.buttonText}>회원가입</Text>
       </TouchableOpacity>
     </View>
