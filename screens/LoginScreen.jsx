8d2fa158a4a2fbcc09c9b4323ec4dcf4d5c4839e
김지나 <jina4066@naver.com>
2024-05-24 04:15:10 +0900

    feat: api 연결

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 import {
   View,
@@ -15,9 +16,52 @@ const LoginScreen = () => {
   const [password, setPassword] = useState("");
 
   const navigation = useNavigation();
-  const handleSignUp = () => {
-    navigation.navigate("SignUp");
+
+  //   const handleSignUp = () => {
+  //     navigation.navigate("SignUp");
+  //   };
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "http://13.209.76.135:8080/auth/sign-in",
+        { email, password }
+      );
+      console.log("로그인 성공");
+      console.log(response.data);
+      alert("로그인 되었습니다.");
+
+      navigation.navigate("home");
+    } catch (error) {
+      console.error(error);
+      alert("로그인 중 오류가 발생했습니다.");
+    }
   };
+
+  const handleSignUp = async () => {
+    try {
+      const response = await axios.post(
+        "http://13.209.76.135:8080/auth/sign-up",
+        {
+          name,
+          email,
+          password,
+        }
+      );
+      // 성공적으로 요청이 완료되면 실행될 로직 (예: 화면 전환, 알림 표시 등)
+      console.log(response.data); // 응답 데이터 로깅
+      alert("회원가입이 완료되었습니다.");
+      // 상태 초기화
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      // 에러 처리 로직
+      console.error(error);
+      alert("회원가입 중 오류가 발생했습니다.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -35,7 +79,7 @@ const LoginScreen = () => {
         secureTextEntry={true}
         autoCapitalize="none"
       />
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>로그인</Text>
       </TouchableOpacity>
       <Button title="계정이 없으신가요?" onPress={handleSignUp} />
