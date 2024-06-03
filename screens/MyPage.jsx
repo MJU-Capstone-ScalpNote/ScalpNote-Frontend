3e3092fe12d7b1c4d2d1932f6700ecdbb4bb4b8a
김지나 <jina4066@naver.com>
2024-06-03 15:45:39 +0900

    feat: mypage 틀만 구현

---
+++ screens/MyPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+
+const MyPage = () => {
+  return (
+    <View>
+      <Text>마이페이지</Text>
+    </View>
+  );
+};
+
+export default MyPage;
