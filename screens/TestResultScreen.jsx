61ac0ab8e71aca878aae48139bdf59648ebd76dc
김지나 <jina4066@naver.com>
2024-06-03 15:46:21 +0900

    feat: 진단내역 홈화면 구현

---
+++ screens/TestResultScreen.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+  Image,
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Results from "../components/Results";
+import StartTest from "../components/StartTest";
+import Footer from "../components/Footer";
+
+const TestResultScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.resultContainer}>
+        <Results />
+        <StartTest />
+      </View>
+
+      <Footer />
+    </View>
+  );
+};
+
+export default TestResultScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  resultContainer: {
+    flex: 1,
+    padding: 20,
+  },
+});
