644ec66236753b179b46964afd99eb168f2ce704
김지나 <jina4066@naver.com>
2024-06-03 15:43:32 +0900

    modify: app.js 수정

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import * as MediaLibrary from "expo-media-library";
-import { Camera } from "expo-camera";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import HomeScreen from "./screens/HomeScreen";
 import SignupScreen from "./screens/SignupScreen";
@@ -14,11 +13,42 @@ import CameraScreen from "./screens/CameraScreen";
 import CommunityScreen from "./screens/CommunityScreen";
 import CommunityHeader from "./components/CommunityHeader";
 import Footer from "./components/Footer";
-import MyPageNotLogined from "./components/MyPageNotLogined";
+import MyPage from "./screens/MyPage";
+import TestResultScreen from "./screens/TestResultScreen";
+import CameraTestScreen from "./screens/CameraTestScreen";
+import GalleryScreen from "./screens/GalleryScreen";
+import WritePostScreen from "./screens/WritePostScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState(null);
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const token = await AsyncStorage.getItem("userToken");
+        if (token) {
+          setUserToken(token);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
   return (
     <View style={styles.container}>
       <NavigationContainer>
@@ -28,6 +58,7 @@ export default function App() {
             component={HomeScreen}
             options={{ title: "홈" }}
           />
+
           <Stack.Screen
             name="Login"
             component={LoginScreen}
@@ -43,22 +74,40 @@ export default function App() {
             component={TestHomeScreen}
             options={{ title: "두피진단" }}
           />
+
           <Stack.Screen
-            name="CameraScreen"
-            component={CameraScreen}
+            name="CameraTestScreen"
+            component={CameraTestScreen}
             options={{ title: "카메라" }}
           />
           <Stack.Screen
-            name="CommunityScreen"
+            name="Gallery"
+            component={GalleryScreen}
+            options={{ title: "갤러리" }}
+          />
+          <Stack.Screen
+            name="Community"
             component={CommunityScreen}
             options={{ title: "커뮤니티" }}
           />
-          <Stack.Screen name="CommunityHeader" component={CommunityHeader} />
           <Stack.Screen
-            name="MyPageNotLogined"
-            component={MyPageNotLogined}
-            options={{ title: "마이페이지" }}
+            name="WritePost"
+            component={WritePostScreen}
+            options={{ title: "글쓰기" }}
           />
+          <Stack.Screen
+            name="TestResult"
+            component={TestResultScreen}
+            options={{ title: "진단내역" }}
+          />
+          <Stack.Screen name="CommunityHeader" component={CommunityHeader} />
+          <Stack.Screen name="MyPage" component={MyPage} />
+
+          {/* {userToken ? (
+            <Stack.Screen name="MyPage" component={MyPage} />
+          ) : (
+            <Stack.Screen name="Login" component={LoginScreen} />
+          )} */}
           <Stack.Screen name="Footer" component={Footer} />
         </Stack.Navigator>
       </NavigationContainer>
