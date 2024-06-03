e4137511e65a137023aee93c61d3233325462e32
김지나 <jina4066@naver.com>
2024-06-03 15:45:24 +0900

    feat: 진단시작화면 갤러리 연동

--- screens/TestHomeScreen.jsx
+++ screens/TestHomeScreen.jsx
@@ -12,15 +12,19 @@ const TestHomeScreen = () => {
   const navigation = useNavigation();
 
   const handleCamera = () => {
-    navigation.navigate("CameraScreen");
+    navigation.navigate("CameraTestScreen");
   };
+  const handleGallery = () => {
+    navigation.navigate("Gallery");
+  };
+
   return (
     <View style={styles.container}>
       <Text>두피가 자세히 보이도록 사진을 찍어주세요!</Text>
       <TouchableOpacity style={styles.button} onPress={handleCamera}>
         <Text style={styles.buttonText}>두피 사진 촬영</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={handleGallery}>
         <Text style={styles.buttonText}>두피 사진 업로드</Text>
       </TouchableOpacity>
     </View>
