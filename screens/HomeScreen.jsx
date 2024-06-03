6c78d99a15440570e0fb0590f7cc08778345df36
김지나 <jina4066@naver.com>
2024-06-03 15:43:48 +0900

    modify: 홈화면 수정

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -11,22 +11,10 @@ import {
   Image,
 } from "react-native";
 import Footer from "../components/Footer";
+import StartTest from "../components/StartTest";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
-
-  const handleLogin = () => {
-    navigation.navigate("Login");
-  };
-  const handleSignUp = () => {
-    navigation.navigate("SignUp");
-  };
-  const handleStartTest = () => {
-    navigation.navigate("TestHomeScreen");
-  };
-  const handleCommunity = () => {
-    navigation.navigate("CommunityScreen");
-  };
 
   return (
     <View style={styles.container}>
@@ -35,20 +23,7 @@ const HomeScreen = () => {
           source={require("../assets/images/Logo.png")}
           style={styles.image}
         />
-        <TouchableOpacity style={styles.testButton} onPress={handleStartTest}>
-          <Text style={styles.testButtonText}>두피 진단 시작</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.testButton} onPress={handleCommunity}>
-          <Text style={styles.testButtonText}>커뮤니티</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.buttonContainer}>
-        <View style={styles.buttonWrapper}>
-          <Button title="로그인" onPress={handleLogin} />
-        </View>
-        <View style={styles.buttonWrapper}>
-          <Button title="회원가입" onPress={handleSignUp} />
-        </View>
+        <StartTest />
       </View>
       <Footer />
     </View>
@@ -71,25 +46,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  testButton: {
-    backgroundColor: "#007AFF",
-    color: "white",
-    paddingVertical: 12,
-    paddingHorizontal: 24,
-    borderRadius: 8,
-    margin: 10,
-  },
-  testButtonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    width: "100%",
-    marginBottom: 20,
   },
 });
 
