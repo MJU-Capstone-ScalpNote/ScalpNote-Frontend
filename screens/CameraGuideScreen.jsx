99d42cb95dbc77517b0f533c0a8700e0be783376
김지나 <jina4066@naver.com>
2024-06-10 23:56:25 +0900

    feat: 가이드 화면 사진 추가

--- screens/CameraGuideScreen.jsx
+++ screens/CameraGuideScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Image } from "react-native";
 import Footer from "../components/Footer";
 
 const CameraGuideScreen = () => {
@@ -16,10 +16,32 @@ const CameraGuideScreen = () => {
           <Text style={styles.guideText}>
             1. 밝은 곳에서 촬영하세요.{"\n\n"}
             2. 두피가 머리카락에 가리지 않도록 정리해주세요.{"\n\n"}
-            3. 두피의 상태를 명확하게 볼 수 있도록 가까이에서 촬영하세요.
+            3. 두피의 상태를 명확하게 진단할 수 있도록 초점을 맞춰주세요.
             {"\n\n"}
             4. 촬영 후 사진이 선명한지 확인하세요.
           </Text>
+          <Text style={styles.guideSubtitle}>잘못 찍은 사진 예시</Text>
+          <View style={styles.imageRow}>
+            <Image
+              source={require("../assets/images/badExample1.png")}
+              style={styles.guideImage}
+            />
+            <Image
+              source={require("../assets/images/badExample2.jpeg")}
+              style={styles.guideImage}
+            />
+          </View>
+          <Text style={styles.guideSubtitle}>잘 찍은 사진 예시</Text>
+          <View style={styles.imageRow}>
+            <Image
+              source={require("../assets/images/goodExample1.png")}
+              style={styles.guideImage}
+            />
+            <Image
+              source={require("../assets/images/goodExample2.png")}
+              style={styles.guideImage}
+            />
+          </View>
         </View>
       </ScrollView>
       <Footer />
@@ -80,8 +102,26 @@ const styles = StyleSheet.create({
   },
   guideText: {
     fontSize: 16,
-    lineHeight: 28,
+    lineHeight: 20,
     color: "#495057",
+    marginBottom: 10,
+  },
+  guideSubtitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginVertical: 10,
+    marginTop: 30,
+    color: "#495057",
+  },
+  imageRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 10,
+  },
+  guideImage: {
+    width: "48%",
+    height: 150,
+    borderRadius: 10,
   },
 });
 
