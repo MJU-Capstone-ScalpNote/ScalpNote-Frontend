a503099a22dbe5b059251d963d4e16fcb1562559
김지나 <jina4066@naver.com>
2024-06-11 15:14:07 +0900

    feat: 글쓰기 버튼 위치 조정

--- screens/CommunityScreen.jsx
+++ screens/CommunityScreen.jsx
@@ -262,7 +262,7 @@ const styles = StyleSheet.create({
   writeButton: {
     position: "absolute",
     right: 20,
-    bottom: 90,
+    bottom: 20,
     width: 60,
     height: 60,
     borderRadius: 30,
